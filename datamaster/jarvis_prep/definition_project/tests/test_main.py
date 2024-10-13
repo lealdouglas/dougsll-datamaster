ebcfd544ecb1a7fb455906232176ca97af8e37d4
Leal <douglas.sleal@outlook.com>
2024-10-13 20:31:57 -0300

    es

--- datamaster/jarvis_prep/definition_project/tests/test_main.py
+++ datamaster/jarvis_prep/definition_project/tests/test_main.py
@@ -6,7 +6,6 @@ from definition_project.main import main
 def test_main(capsys):
     # Chama a função main
     main()
-    
     # Captura a saída padrão
     captured = capsys.readouterr()
     # Verifica se a saída é 'hello world\n'
